5efb2336f6c9ebfd23257ec8e27572197758b200
mengde <huai.mengde@gmail.com>
2025-07-05 20:12:06 +0200

    open yfinaces in headless mode

--- NvidiaPredictions/stock_scraper.py
+++ NvidiaPredictions/stock_scraper.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def scrape_stock_price():
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless=new")
+    options.add_argument("window-size=1920,1080")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(options=options)
 
